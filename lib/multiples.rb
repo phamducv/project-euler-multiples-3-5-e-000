8c22a8e900eb7a9bf63ac25972abc857811469bb
phamducv <phamducv@gmail.com>
2015-12-04 20:59:30 +0000

    nitrous-backup at Fri Dec  4 20:59:30 UTC 2015

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,19 @@
 # Enter your procedural solution here!
+sum=0
+limit=[*1..100]
+def collect_multiples(limit)
+
+
+
+        limit.each.do |number|
+                if number % 3 == 0
+
+                        sum+=number/3
+
+                elsif number % 5 == 0
+                        sum+=number/5
+
+
+  end
+end
+puts collect_multiples(limit)
